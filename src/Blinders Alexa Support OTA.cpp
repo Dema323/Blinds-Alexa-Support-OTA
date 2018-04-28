8e1abfee30fd662bc5ea2a4efe84d34d77ee52ed
Alexandre Logist <logist.alex@live.be>
2018-04-28 18:35:53 +0200

    added time

--- src/Blinders Alexa Support OTA.cpp
+++ src/Blinders Alexa Support OTA.cpp
@@ -1,10 +1,11 @@
-#include <ArduinoOTA.h>
-#include <Arduino.h>
-#define BLYNK_PRINT Serial
-
-
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <ArduinoOTA.h>
+#include <Arduino.h>
+#include <TimeLib.h>
+#include <WidgetRTC.h>
+
+#define BLYNK_PRINT Serial
 
 char auth[] = "c1b5ca348c734e5fabd12fb4cceba4b2";
 
@@ -12,19 +13,40 @@ char ssid[] = "WiFi-2.4-D4F0";
 char pass[] = "A26YXkP73nuz";
 
 BlynkTimer timer;
+WidgetTerminal terminal(V8);
+WidgetRTC rtc;
 
-int Height;
+int eventTime;
 
-void UPHeightCounter();
-void DOWNHeightCounter();
+void StartUp();
+void StopUp();
+void StartDown();
+void StopDown();
 void WriteVirtual();
 
-void UPHeightCounter(){
-  Height++;
+void StartUp(){
+  eventTime = huidige tijd
+  String currentTime = String(hour()) + ":" + minute() + ":" + second() + " - Started moving up";
+  terminal.println(currentTime);
+  terminal.flush();
 }
 
-void DOWNHeightCounter(){
-  Height--;
+void StopUp(){
+  String currentTime = String(hour()) + ":" + minute() + ":" + second() + " - Stopped moving up";
+  terminal.println(currentTime);
+  terminal.flush();
+}
+
+void StartDown(){
+  String currentTime = String(hour()) + ":" + minute() + ":" + second() + " - Started moving down";
+  terminal.println(currentTime);
+  terminal.flush();
+}
+
+void StopDown(){
+  String currentTime = String(hour()) + ":" + minute() + ":" + second() + " - Stopped moving down";
+  terminal.println(currentTime);
+  terminal.flush();
 }
 
 BLYNK_WRITE(V2) //DOWN
@@ -33,9 +55,10 @@ BLYNK_WRITE(V2) //DOWN
   digitalWrite(12, LOW);
   Blynk.virtualWrite(V3, LOW);
   delay(1000);
+  StartDown();
   digitalWrite(13, HIGH);
-  timer.setInterval(1000, DOWNHeightCounter);
  }else{
+  StopDown();
   digitalWrite(13, LOW);
  }
  }
@@ -45,9 +68,10 @@ BLYNK_WRITE(V2) //DOWN
   digitalWrite(13, LOW);
   Blynk.virtualWrite(V2, LOW);
   delay(1000);
+  StartUp();
   digitalWrite(12, HIGH);
-  timer.setInterval(1000, UPHeightCounter);
  }else{
+  StopUp();
   digitalWrite(12, LOW);
  }
  }
@@ -62,20 +86,28 @@ BLYNK_WRITE(V2) //DOWN
  }
 
  void WriteVirtual(){
-   Blynk.virtualWrite(V10, Height);
+   Blynk.virtualWrite(V10, 25);
  }
 
 void setup()
 {
+  setSyncInterval(1);
   WiFi.mode(WIFI_STA);
   Blynk.begin(auth, ssid, pass);
-  while (Blynk.connect() == false) {}
+  while (Blynk.connect() == false) {
+    delay(1000);
+  }
+  setSyncInterval(900000);
   ArduinoOTA.begin();
+  rtc.begin();
   timer.setInterval(500, WriteVirtual);
+  terminal.println("Device online");
+  terminal.flush();
 }
 void loop()
 {
   Blynk.run();
   ArduinoOTA.handle();
   timer.run();
+
 }
